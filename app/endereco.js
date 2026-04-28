abda12338fcc052a05db81f0bf33a1db059c1027
Gabriel Sobral <ggabrielssobral@gmail.com>
2026-04-28 17:47:41 -0300

    feat: add routing components for home, cart, profile, and order tracking screens

--- app/endereco.js
+++ app/endereco.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import EnderecoScreen from '../src/screens/EnderecoScreen';
-//Só um teste para ver se conseguir
-export default function EnderecoRoute() {
+
+export default function RotaEndereco() {
   return <EnderecoScreen />;
 }
